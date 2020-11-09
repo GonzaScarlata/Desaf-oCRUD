a5749ea188a294087ac8048d70e6b07065be7f02
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2020-11-09 13:07:09 -0300

    modal agregado

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const formReminder = document.getElementById('formReminder');
 const taskNameInput = document.getElementById('taskName');
 const taskTypeInput = document.getElementById('taskType');
-const taskDescriptionInput= document.getElementById('taskDescription');
+const taskDescriptionInput = document.getElementById('taskDescription');
 const taskDateInput = document.getElementById('taskDate');
 const tasksList = document.getElementById('taskList');
 const taskTable = document.getElementById('taskTable');
 console.log("taskTable", taskTable)
 
-const generateId = function () {   
+const generateId = function () {
     return '_' + Math.random().toString(36).substr(2, 9);
-}; 
+};
 
 formReminder.onsubmit = (event) => {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -40,15 +40,42 @@ function displayTasks() {
     for (let index = 0; index < tasks.length; index++) {
         const task = tasks[index];
 
-     const tr = `
+        const tr = `
         <tr>
             <th scope="row">${index + 1}</th>
             <td>${task.taskName}</td>
             <td>${task.taskType}</td>
             <td>${task.taskDescription}</td>
-            <td>@${task.taskDate}</td>
+            <td>${task.taskDate}</td>
+            <td>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal${task.id}">
+                Mostrar
+                </button>
+                
+                <!-- Modal -->
+                <div class="modal fade" id="modal${task.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </td>
         </tr>
-        `   ; 
+        `   ;
         rows.push(tr);
     }
     taskTable.innerHTML = rows.join('');
